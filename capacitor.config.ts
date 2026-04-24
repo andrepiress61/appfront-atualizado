a032fbb2d5ed713be35da703f55969e9da31ae1f
andrepiress61 <andre.f.mesquita6@aluno.senai.br>
2026-04-24 10:02:16 -0300

    quinto commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'br.com.ecototally.app',
   appName: 'EcoTotally',
-  webDir: 'dist'
+  webDir: 'dist/ectotally/browser',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
